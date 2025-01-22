72516e05f9442e08f8aca954bf298da87f84bece
Ashish Singh <ashishsinghrana39@gmail.com>
2025-01-22 17:46:16 +0530

    committed

---
+++ Fourth.java
@@ -0,0 +1,9 @@
+public class Fourth{
+	public static void main(String[] args){
+		double cp=129;
+		double sp=191;
+		double profit=sp-cp;
+		double percentage=((sp-cp)/cp)*100;
+		System.out.println("Profit is "+profit + "\nProfit percentage is " + profit );
+	}
+}
